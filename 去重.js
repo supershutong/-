0412a740775f736886aa1173d4f74a90097a4cfd
刘向阳 <liuxiangyang@cslc.com.cn>
2021-03-05 22:57:08 +0800

    去重

---
+++ 去重.js
@@ -0,0 +1,42 @@
+// 1、双层循环
+function unique(arr) {
+  if (arr.length <= 1) return arr
+  let res = [arr[0]]
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < res.length; j++) {
+      if (arr[i] === res[j]) break
+      if (j === res.length - 1) res.push(arr[i])
+    }
+  }
+  return res
+}
+
+// 2、indexOf
+function unique(arr) {
+  let res = []
+  for (let i = 0; i < arr.length; i++) {
+    if (res.indexOf(arr[i]) === -1) res.push(arr[i])
+  }
+  return res
+}
+
+// 3、filter + indexOf
+function unique(arr) {
+  return arr.filter((item, index) => {
+    return arr.indexOf(item) === index
+  })
+}
+
+// 4、Set
+function unique(arr) {
+  return [...new Set(arr)]
+}
+
+// 5、Map
+function unique(arr) {
+  let remain = new Map()
+  return arr.filter(item => !remain.has(item) && remain.set(item, true))
+}
+
+let arr = [2, 1, 3, 5, 3, 3, 7, 4, -6]
+console.log(unique(arr))
