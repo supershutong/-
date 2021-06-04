de9529e6931347628b8d63eb5b2f21fa3a5c5920
liuxiangyang <liuxiangyang@cslc.com.cn>
2021-06-04 20:11:29 +0800

    jsonp优化

--- JSONP.js
+++ JSONP.js
@@ -1,24 +1,24 @@
 // JSONP：常见的跨域手段，利用 <script> 标签没有跨域限制的漏洞，来达到与第三方通讯的目的。
 
-function jsonp({ url, params, callbackName }) {
+function jsonp(url, params, callbackName) {
   const generateURL = () => {
-    let dataStr = ''
-    for (let key in params) {
-      dataStr += `${key}=${params[key]}&`
+    let queryString = ''
+    for (const key in params) {
+      if (Object.hasOwnProperty.call(params, key)) {
+        queryString += `${key}=${params[key]}&`
+      }
     }
-    dataStr += `callback=${callbackName}`
-    return `${url}?${dataStr}`
+    return `${url}?${queryString}callback=${callbackName}`
   }
 
   return new Promise((resolve, reject) => {
-    callbackName = callbackName || Math.random().toString()
-    let scriptEle = document.createElement('script')
-    scriptEle.src = generateURL()
-    document.body.appendChild(scriptEle)
+    let el = document.createElement('script')
+    el.src = generateURL()
+    document.body.appendChild(el)
     // 服务器返回字符串 `${callbackName}(${服务器的数据})`，浏览器解析即可执行。
     window[callbackName] = data => {
       resolve(data) // 浏览器解析服务器返回数据
-      document.body.removeChild(scriptEle) // 清除插入的dom
+      document.body.removeChild(el) // 清除插入的dom
     }
   })
 }
